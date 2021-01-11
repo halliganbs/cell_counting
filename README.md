a788499ab1d3f76aa27c5fce95fb17abfc8516f0
Ben <bshalligan@gmail.com>
2021-01-11 14:21:30 -0500

    updated documentation

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # cell_counting
 Implementation of Cellpose in order to count cells
+
+## Installation
+
+I'll create a conda `requirements.txt` later but for now run it til it breaks and install the `pip` packages it asks for
+
+## Running
+
+- Move data into `/data` folder in parent directory
+- Start Jupyter notebook in project directory
+- modify all variable values for data
+- run first code cell and select diretories
+- run rest of cells
+
+## Help
+
+bhalliga@umich.edu pls don't
--- counter.py
+++ counter.py
@@ -15,12 +15,16 @@ from progress.bar import Bar
 
 REG = r'\/([0-9]*)\/3765_([A-Z][0-9]{2})_T0001F001L01A01Z01C01' # regex for metadata
 
-def get_wellID(filename, reg=REG):
-    wellId = re.findall(reg, filename)
-    return wellId[0]
-
 def get_data(path, model, reg, dim=30):
     """
+    generates counts from single image
+    args:
+        path - path to data directory
+        model - cellpose model
+        reg - REGEX for meta data
+        dim - cell size based on pixels default 30
+    returns:
+        df - dataframe of singel image with meta data counts
     """
     files = glob.glob(path+'/*.tiff')
     data = {
@@ -46,6 +50,14 @@ def get_data(path, model, reg, dim=30):
 
 def gen_df(dirs, model, reg, dim=None):
     """
+    generates dataframe
+    args:
+        dirs - parent directory of data
+        model - cellpose model
+        reg - regex
+        dim - dimensions of cell None for 30
+    returns:
+        df - dataframe of meta data and cell counts
     """
     col_names = ['filename','wellID','counts']
     df = pd.DataFrame(columns=col_names)
@@ -60,6 +72,17 @@ def gen_df(dirs, model, reg, dim=None):
     return df
 
 def gen_boxplot(df, index, vals, cols, title='heatmap'):
+    """
+    generates and saves a heatmap
+    args:
+        df - dataframe of meta data and args
+        index - index of heatmap
+        vals - values for heatmap to color square based on
+        cols - columns of heatmap
+        title - title of heatmap default heatmap
+    returns:
+        None
+    """
     dh = pd.pivot_table(data=df, index=index, values=vals, columns=cols)
     heat = sns.heatmap(dh)
     heat.set_title(title)
